26644903fbb8be39bf5d645ed70729e52baf083b
Your Name <dilliram2003adhikari@gmail.com>
2026-04-26 16:02:08 +0530

    increment-test-written

--- client/index.test.ts
+++ client/index.test.ts
@@ -1,11 +1,11 @@
-import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction, TransactionInstruction } from "@solana/web3.js";
 import { expect, test } from "bun:test";
-import { CounterSize, schema } from "./type";
+import { CounterSize, DecrementInstruction, decrementInstructionSchema, IncrementInstruction, incrementInstructionSchema, schema } from "./type";
 import * as borsh from "borsh"
 
 let dataAccount:Keypair;
 let adminAddress:Keypair;
-const programId=new PublicKey("6zEP9epe6q1p98ntG43rAtaazgcHWcNweb89FiTxAz5z")
+const programId=new PublicKey("4XTzbbeVNsspRwzSfdYGJGV3R8Qgtqas7BCuEePcRmdx")
 test("Account initialization", async()=>{
    dataAccount=Keypair.generate();
    adminAddress=Keypair.generate();
@@ -40,4 +40,67 @@ test("Account initialization", async()=>{
 })
 
 
+test("Increment value", async()=>{
+const connection=new Connection("http://127.0.0.1:8899");
+ 
+function incrementDataOnchain(vlaue:number){
+   const data= borsh.serialize(
+      incrementInstructionSchema,
+      new IncrementInstruction(vlaue)
+   )
+   return new TransactionInstruction({
+    keys: [
+      { pubkey: dataAccount.publicKey, isSigner: false, isWritable: true },
+    
+    ],
+    programId: programId,
+    data:data
+  })
+}
+
+const ix= incrementDataOnchain(4);
+const tx=new Transaction();
+tx.add(ix);
+const signature=await connection.sendTransaction(tx,[adminAddress])
+await connection.confirmTransaction(signature);
+
+const dataAccountInfo=await connection.getAccountInfo(dataAccount.publicKey);
+const counter= await borsh.deserialize(schema,dataAccountInfo?.data);
+console.log(counter.count);
+
+expect(counter.count).toBe(4)
+
+
+
+   
+ 
+})
+
+
+test("decrement value",async()=>{
+   const connection=new Connection("http://127.0.0.1:8899");
+   function decrementValueOnchain(value:number){
+   const data=borsh.serialize(
+      decrementInstructionSchema,
+      new DecrementInstruction(value)
+   )
+
+   return new TransactionInstruction({
+      keys:[{ pubkey: dataAccount.publicKey, isSigner: false, isWritable: true }],
+      programId: programId,
+      data:data
+   })
+
+}
+
+const ix2=decrementValueOnchain(2);
+const tx2=new Transaction();
+tx2.add(ix2);
+const signature2=await connection.sendTransaction(tx2,[adminAddress]);
+await connection.confirmTransaction(signature2);
+
+const dataAccountDecrementInfo=connection.getAccountInfo(dataAccount.publicKey);
+const decrementCounter=await borsh.deserialize(schema, dataAccountDecrementInfo?.data);
+console.log(decrementCounter.count);
+})
 
